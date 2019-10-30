2b85dbc63c4cfbd9be293ea603d0fb9d32b52964
Michael Kirsch <michaelskirsch@gmail.com>
2019-10-30 13:08:51 -0600

    added tests

--- afternoon-hw/app.js
+++ afternoon-hw/app.js
@@ -15,10 +15,20 @@ app.get('/apps', (req, res) => {
   if ('sort' in req.query) {
     if (req.query.sort == 'app') {
       // sort by App (app name)
-      sortedData = appsData.sort((a, b) => (a.App > b.App ? 1 : -1));
+      sortedData = appsData.sort((a, b) =>
+        a.App > b.App ? 1 : a.App === b.App ? 0 : -1
+      );
     } else if (req.query.sort == 'rating') {
       // sort by rating
-      sortedData = appsData.sort((a, b) => (a.Rating > b.Rating ? 1 : -1));
+      sortedData = appsData.sort((a, b) => {
+        if (a.Rating - b.Rating) {
+          return a.Rating - b.Rating;
+        }
+        return a.App > b.App ? 1 : -1; //app names should never be identical, so useful tiebreaker
+      });
+    } else {
+      res.status(400).send("Sort must be one of 'app' and 'rating' ");
+      return;
     }
   } else {
     sortedData = appsData;
@@ -27,13 +37,15 @@ app.get('/apps', (req, res) => {
   if ('genre' in req.query) {
     if (
       ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].indexOf(
-        req.query.genre) >= 0
+        req.query.genre
+      ) >= 0
     ) {
       sortedData = sortedData.filter(item =>
         item.Genres.toLowerCase().includes(req.query.genre)
       );
     } else {
-      res.status(400).json({ error: 'genre does not exist' });
+      res.status(400).send('genre does not exist');
+      return;
     }
   }
   res.status(200).json(sortedData);
--- afternoon-hw/test/app.test.js
+++ afternoon-hw/test/app.test.js
@@ -1,32 +1,82 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const app = require('../app');
 const data = require('../playstore');
 const supertest = require('supertest');
+const store = require('../playstore');
 
 describe('GET /playstore', () => {
-    it('should return with 200', () => {
-        return supertest(app)
-            .get('/apps')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .then(res => {
-                expect(res.body).to.deep.equal(data);
-            });
-    });
+  it('should return with 200', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.deep.equal(data);
+      });
+  });
 
-    it('should return by rating', () => {
-        return supertest(app)
-            .get('/apps')
-            .query({sort: 'rating'})
-            .then(res => {
-                expect(res.body)
-            })
-    })
+  it('should return properly sorted by rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'rating' })
+      .then(res => {
+        expect(res.body).eql(
+          store.sort((a, b) => {
+            if (a.Rating - b.Rating) {
+              return a.Rating - b.Rating;
+            }
+            return a.App > b.App ? 1 : -1; //app names should never be identical, so useful tiebreaker
+          })
+        );
+      });
+  });
+  it('should return properly sorted by app name', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'app' })
+      .then(res => {
+        expect(res.body).eql(store.sort((a, b) => (a.App > b.App ? 1 : -1)));
+      });
+  });
 
-    it('should return with 400 error', () => {
-        return supertest(app)
-            .get('/apps')
-            .query({sort: 'INVALID'})
-            .expect(400, 'must one of genre and rating ')
-    })
+  it('should return a filtered list if given a genre', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genre: 'casual' })
+      .then(res => {
+        expect(res.body).eql(
+          store.filter(app => app.Genres.toLowerCase().includes('casual'))
+        );
+      });
+  });
+
+  it('should return a sorted, filtered list if given a genre and sort', () => {
+    let expected = store
+      .filter(app => app.Genres.toLowerCase().includes('casual'))
+      .sort((a, b) => {
+        if (a.Rating - b.Rating) {
+          return a.Rating - b.Rating;
+        }
+        return a.App > b.App ? 1 : -1; //app names should never be identical, so useful tiebreaker
+      });
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'rating', genre: 'casual' })
+      .then(res => {
+        expect(res.body).eql(expected);
+      });
+  });
+
+  it('should return with 400 error when sort parameter is invalid', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'INVALID' })
+      .expect(400, "Sort must be one of 'app' and 'rating' ");
+  });
+  it('should return with 400 error when genre parameter is invalid', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genre: 'invalid' })
+      .expect(400, 'genre does not exist');
+  });
 });
